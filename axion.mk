62a5ee6df1e12620ae08d300d4e0c120744bc457
TheAttila <417474696C61@gmail.com>
2025-04-16 22:44:06 -0300

    first commit

---
+++ axion.mk
@@ -0,0 +1,17 @@
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/BoardConfigLineage.mk)
+$(call inherit-product, vendor/lineage/config/BoardConfigSoong.mk)
+$(call inherit-product, device/lineage/sepolicy/common/sepolicy.mk)
+-include vendor/lineage/build/core/config.mk
+
+BOARD_EXT4_SHARE_DUP_BLOCKS := true
+
+TARGET_NO_KERNEL_OVERRIDE := true
+TARGET_NO_KERNEL_IMAGE := true
+
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_BLUR := true
+
+AXION_MAINTAINER := TheAttila
+
+WITH_GMS := true
